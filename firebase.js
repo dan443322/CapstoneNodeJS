35167211482dc6ceb8e4bea99876a5340fec3199
dan-sprobe-intern <tindoy.dan@sprobe.com>
2024-04-02 18:02:24 +0800

    hahaha

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("./service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -11,19 +11,23 @@ admin.initializeApp({
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCSPNk3g0Ri_bpCHlf3mXy5j-Bg8ReeUjA",
-  authDomain: "capstone-ffb9d.firebaseapp.com",
-  databaseURL:
-    "https://capstone-ffb9d-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "capstone-ffb9d",
-  storageBucket: "capstone-ffb9d.appspot.com",
-  messagingSenderId: "248293716228",
-  appId: "1:248293716228:web:810ba37d3ad508122fe051",
-};
+const db = admin.firestore();
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+let testUser = db.collection("sSHMEXk1Jv-4tmmlc3xS37ANZqtRgx_t");
 
-module.exports = app;
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCSPNk3g0Ri_bpCHlf3mXy5j-Bg8ReeUjA",
+//   authDomain: "capstone-ffb9d.firebaseapp.com",
+//   databaseURL:
+//     "https://capstone-ffb9d-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "capstone-ffb9d",
+//   storageBucket: "capstone-ffb9d.appspot.com",
+//   messagingSenderId: "248293716228",
+//   appId: "1:248293716228:web:810ba37d3ad508122fe051",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// module.exports = app;

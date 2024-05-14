c0864eb0dee22208394504f0e2b4d566184d941c
ARLU <arludave23@gmail.com>
2024-05-14 21:10:50 +0800

    he

--- api.service.js
+++ api.service.js
@@ -4,6 +4,21 @@ const statusUrl = "https://sgp1.blynk.cloud/external/api/isHardwareConnected";
 const getAllUrl = "https://sgp1.blynk.cloud/external/api/getAll";
 const token = process.env.BLYNK_API_KEY;
 
+const resetTotalVolume = async () => {
+  try {
+    await axios.get("https://sgp1.blynk.cloud/external/api/update", {
+      params: {
+        token: token,
+        pin: "V7",
+        value: 0,
+      },
+    });
+    console.log("Total Water Value Reset Successfully");
+  } catch (err) {
+    console.log("Error resetting Total Value:", err);
+  }
+};
+
 const fetchAll = async () => {
   console.log("Fetching all Data...");
   try {
